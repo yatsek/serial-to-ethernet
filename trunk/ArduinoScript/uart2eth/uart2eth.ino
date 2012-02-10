f4ab2a9ba9fe128a5c2e9bf9751d9128a9e8f3dd
lds133@gmail.com <lds133@gmail.com@d2bbee53-d93b-5bc9-50af-a679490f0ee9>
2012-02-10 14:01:11 +0000

    initial checkout

---
+++ trunk/ArduinoScript/uart2eth/uart2eth.ino
@@ -0,0 +1,251 @@
+#include <dhcp.h>
+#include <dnslkup.h>
+#include <enc28j60.h>
+#include <EtherShield.h>
+#include <ip_arp_udp_tcp.h>
+#include <ip_config.h>
+#include <net.h>
+#include <websrv_help_functions.h>
+
+
+#include <EEPROM.h>
+#include <avr/wdt.h>
+
+
+
+uint8_t _mac[6] = {0x54,0x55,0x58,0x10,0x00,0x25}; 
+
+uint8_t _ip[4] = {192,168,0,133};
+
+
+#define PORT 80
+#define BUFFER_SIZE 550
+
+
+
+#define PIN_BLUE    2
+#define PIN_GREEN   3
+#define PIN_RED     4
+#define PIN_YELLOW  5 
+#define SETTINGSADDR     0
+#define NOSETTINGS       0xFF
+
+struct SETTINGS
+{
+  byte Flag;
+  long Baud;
+};
+
+
+SETTINGS _settings;
+uint8_t _buf[BUFFER_SIZE];
+char _sbuf[BUFFER_SIZE+1];
+int _sbufsize=0;
+boolean _sbufoverflow=false;
+boolean _uartstarted=false;
+
+
+EtherShield _es=EtherShield();
+
+
+
+#define ANIMATIONSPEED 200
+void StartupAnimation(int value)
+{
+  digitalWrite(PIN_BLUE, value);
+  delay(ANIMATIONSPEED);
+  digitalWrite(PIN_GREEN, value);
+  delay(ANIMATIONSPEED);
+  digitalWrite(PIN_RED, value);
+  delay(ANIMATIONSPEED);
+  digitalWrite(PIN_YELLOW, value);
+  delay(ANIMATIONSPEED);
+  
+
+}
+
+void setup()
+{
+  pinMode(PIN_BLUE,  OUTPUT); 
+  pinMode(PIN_GREEN, OUTPUT);
+  pinMode(PIN_RED,   OUTPUT);
+  pinMode(PIN_YELLOW,  OUTPUT);
+
+
+  StartupAnimation(HIGH);
+
+
+  _es.ES_enc28j60SpiInit();
+  _es.ES_enc28j60Init(_mac);
+  
+
+  
+  _es.ES_init_ip_arp_udp_tcp(_mac,_ip, PORT);
+  
+  StartupAnimation(LOW);    
+
+  SettingsRead(SETTINGSADDR,&_settings,sizeof(SETTINGS));
+  if (_settings.Flag == NOSETTINGS)
+  {
+        _settings.Flag =0;
+        _settings.Baud =0;
+        SettingsWrite(SETTINGSADDR,&_settings,sizeof(SETTINGS));
+  } else
+  {
+      if (_settings.Baud !=0)
+      {
+         Serial.begin(_settings.Baud);
+         _uartstarted=true;
+         digitalWrite(PIN_GREEN, HIGH);
+      }
+  }
+  
+  
+  wdt_enable(WDTO_8S);
+}
+
+
+long _t=0;
+
+void CheckBuffer()
+{
+  static char ch;
+  
+  if (!_uartstarted) return;
+  
+  while (Serial.available() > 0) 
+  {
+      ch = Serial.read();
+      if (ch==0) continue;
+      if (!_sbufoverflow) 
+      {    _sbuf[_sbufsize++]=ch;
+          analogWrite(PIN_YELLOW, 100);
+      }
+      if (_sbufsize>=BUFFER_SIZE) 
+      {   _sbuf[BUFFER_SIZE]=0; 
+          _sbufoverflow =true;
+          digitalWrite(PIN_RED, HIGH);
+          break;
+      }
+  }
+  if (!_sbufoverflow) _sbuf[_sbufsize]=0;
+
+}
+
+void ClearBuffer()
+{
+  memset(_sbuf,0,BUFFER_SIZE+1);
+  _sbufsize=0;
+  _sbufoverflow =false;
+  digitalWrite(PIN_YELLOW, LOW);
+  digitalWrite(PIN_RED, LOW);
+  
+}
+
+
+void ProcessCmd(uint16_t  dat_p)
+{
+  long i;
+  
+  
+  if (strncmp("HELP",(char *)&(_buf[dat_p]),4)==0)
+  {
+    sprintf(_sbuf,"\r\n--- UART2ETH\r\nSTART <baud>\r\nSTOP\r\nGET\r\nPUT <data>\r\nCLEAR\r\nHELP\r\n");
+    return;
+  } 
+  
+  if (strncmp("GET",(char *)&(_buf[dat_p]),3)==0)
+  {
+    return;
+  }
+
+  if (strncmp("PUT",(char *)&(_buf[dat_p]),3)==0)
+  {
+    if (!_uartstarted)
+    {  sprintf(_sbuf,"\r\n--- Send START first\r\n");
+    } else
+    {  sprintf(_sbuf,"\r\n--- ");
+       
+       i=0;
+       while(_buf[i+4+dat_p]!=0)
+       {  Serial.write(_buf[i+4+dat_p]); 
+          _sbuf[i+6] = _buf[i+4+dat_p];
+          i++;
+       }
+       i++;
+       _sbuf[i+6] = 0;
+       
+    }
+    return;
+  }
+ 
+  
+  if (strncmp("START ",(char *)&(_buf[dat_p]),6)==0)
+  {
+    i=0;
+    sscanf ((char *)&(_buf[dat_p+6]),"%ld",&i);
+    Serial.begin(i);
+    sprintf(_sbuf,"\r\n--- Started %ld\r\n",i);
+    _uartstarted=true;
+    digitalWrite(PIN_GREEN, HIGH);
+    _settings.Baud =i;
+    SettingsWrite(SETTINGSADDR,&_settings,sizeof(SETTINGS));    
+    return;
+  }
+  
+  if (strncmp("STOP",(char *)&(_buf[dat_p]),4)==0)
+  {
+     _uartstarted=false;
+     ClearBuffer();
+     Serial.end() ;
+     sprintf(_sbuf,"\r\n--- Stopped\r\n");
+     digitalWrite(PIN_GREEN, LOW);
+     _settings.Baud =0;
+     SettingsWrite(SETTINGSADDR,&_settings,sizeof(SETTINGS));        
+     return;
+  }
+  
+  if (strncmp("CLEAR",(char *)&(_buf[dat_p]),5)==0)
+  {
+     sprintf(_sbuf,"\r\n--- Cleared\r\n");
+     return;
+  }
+
+  sprintf(_sbuf,"\r\n--- Unknown command\r\n");
+  
+}
+
+
+void loop()
+{
+  uint16_t  dat_p;
+  
+  wdt_reset();  
+
+  digitalWrite(PIN_BLUE, LOW);
+  CheckBuffer();
+  dat_p=_es.ES_packetloop_icmp_tcp(_buf,_es.ES_enc28j60PacketReceive(BUFFER_SIZE,_buf));
+
+  if(dat_p==0)  return;
+  digitalWrite(PIN_BLUE, HIGH);  
+ 
+  ProcessCmd(dat_p);
+    
+  dat_p=_es.ES_fill_tcp_data(_buf,0,_sbuf);
+  ClearBuffer();
+  _es.ES_www_server_reply(_buf,dat_p); 
+
+}
+
+void SettingsRead(int eepromaddr,void* addr,int len)
+{
+    for(int i=0;i<len;i++)
+      *(((byte*)addr)+i) = EEPROM.read(i+eepromaddr);
+}
+
+
+void SettingsWrite(int eepromaddr,void* addr,int len)
+{
+    for(int i=0;i<len;i++)
+      EEPROM.write(i+eepromaddr,*(((byte*)addr)+i));
+}
---
+++ trunk/ClientApplication/ArduinoTCPClient/Form1.cs
@@ -0,0 +1,331 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Net.Sockets;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Net;
+using System.Threading;
+
+namespace ArduinoTCPClient
+{
+    public partial class Form1 : Form
+    {
+        
+        const string IP = "192.168.0.133";
+
+
+        const int PORT =80;
+        const string ERROR = "SOCKET ERROR";
+        StreamWriter logfile = 	null;
+
+
+        
+        int _counter=0;
+        List<ListViewItem> _data = new List<ListViewItem>();
+        ListViewItem _nullitem;
+        const int MAXTAILSIZE = 10000;
+        string  _tail=null;
+
+        public Form1()
+        {
+            InitializeComponent();
+            listView1.VirtualListSize = 0;
+            _nullitem = new ListViewItem();
+            _nullitem.Text = "NULL";
+            ListViewItem.ListViewSubItem lvsi = new ListViewItem.ListViewSubItem(); 
+            _nullitem.SubItems.Add(lvsi); 		
+
+
+
+
+
+
+        }
+
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            cbBaud.Text = "9600";
+            CheckLogFile();
+
+        }
+
+
+        #region SEND COMMAND
+
+        void a_Completed(object sender, SocketAsyncEventArgs e)
+        {
+            completed = true;
+        }
+
+
+        const long SEC3 = 10000*1000*3;
+
+        const int SENDBUFSIZE=20;
+        const int RECVDBUFSIZE=2048;
+        ASCIIEncoding  encoding=new System.Text.ASCIIEncoding();
+        byte[] recv=new byte[RECVDBUFSIZE];
+        byte[] send = new byte[SENDBUFSIZE];
+        bool completed;
+        void Command(string cmd)
+        {
+            Socket server =null;
+            try
+            {
+                IPEndPoint ipep =     new IPEndPoint(IPAddress.Parse(IP), PORT);
+                server = new Socket(AddressFamily.InterNetwork,SocketType.Stream, ProtocolType.Tcp);
+                server.Connect(ipep);
+
+                byte[] data = encoding.GetBytes(cmd);
+                //for(int i=0;i<SENDBUFSIZE;i++) send[i] = 32;
+                Array.Clear(send,0,SENDBUFSIZE);
+                Array.Copy(data,send,data.Length>SENDBUFSIZE ? SENDBUFSIZE : data.Length);
+
+                server.Send(send);
+
+                Array.Clear(recv,0,RECVDBUFSIZE);
+                //server.Receive(recv);
+                SocketAsyncEventArgs a = new SocketAsyncEventArgs();
+                a.SetBuffer(recv,0,RECVDBUFSIZE);
+                a.Completed += new EventHandler<SocketAsyncEventArgs>(a_Completed);
+                completed = false;
+                
+                a.SocketFlags = SocketFlags.None;
+                
+
+                server.ReceiveAsync(a);
+
+
+                long t = DateTime.Now.Ticks;
+                
+                while (DateTime.Now.Ticks-t<SEC3)
+                {
+                    Thread.Sleep(1);
+                    if (completed) break; 
+                    
+                }
+
+                if (!completed) throw new Exception("Timeout");
+                if (a.BytesTransferred!=0)
+                {
+                    string str = encoding.GetString(recv, 0, recv.Length);
+                    ProcessString(str);
+                }
+
+                server.Close();
+            }
+            catch
+            {
+
+                InsertItem(ERROR);
+                if (server!=null) server.Close();
+            }
+
+        }
+
+        #endregion
+
+
+        void InsertItem(string s)
+        {
+            if (string.IsNullOrEmpty(s)) return;
+
+            ListViewItem lvi = new ListViewItem(); 	
+            lvi.Text = s;
+            
+            ListViewItem.ListViewSubItem lvsi = new ListViewItem.ListViewSubItem(); 
+            lvi.SubItems.Add(lvsi); 			
+            _data.Add(lvi);
+
+            listView1.VirtualListSize = _data.Count;   
+            listView1.Update();
+
+            if (cbAutoscroll.Checked)
+                listView1.EnsureVisible(listView1.VirtualListSize-1);
+        
+        }
+
+
+        char[] SPLIT = new char[]{'\n'};
+        char[] TRIM = new char[]{'\n','\r',' ','\t','\0'};
+        char[] TRIM0 = new char[]{'\0'};
+
+        void ProcessString(string str)
+        {
+            str = str.Trim(TRIM0);
+
+            if (logfile!=null)
+            {
+                logfile.Write(str);
+                logfile.Flush();
+            }
+
+            if (_tail!=null)
+            {
+                str = _tail+str;
+                _tail = null;
+            }
+
+            if (!str.EndsWith("\n"))
+            {
+                int n = str.LastIndexOf("\n");
+                if (n==-1)
+                {   _tail = str;
+                    return;
+                }
+
+                _tail = str.Substring(n+1);
+                str = n==0 ? string.Empty : str.Substring(0,n);
+
+
+            }
+
+            string[] split = str.Split(SPLIT);
+            foreach(string s in split)
+                InsertItem(s.Trim(TRIM));
+
+
+            tbInfo.Text = _tail==null ? string.Empty : _tail;
+
+        
+        }
+
+
+        void Clear()
+        {
+            _data.Clear();
+            _counter =0;
+            timer1.Stop();
+            listView1.VirtualListSize=0;
+            tbInfo.Text = string.Empty;
+            Refresh();
+        }
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Command("START "+cbBaud.Text);
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            label1.Text =  (_counter++).ToString();
+            Command("GET");
+            
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            
+            Clear();
+            Command("CLEAR");
+            timer1.Start();
+        }
+
+        private void listView1_RetrieveVirtualItem(object sender, RetrieveVirtualItemEventArgs e)
+        {
+            if ((e==null) || (e.ItemIndex>=_data.Count) || (e.ItemIndex<0) || (_data==null)) 
+            {   e.Item = _nullitem;
+                return;
+            }
+
+            e.Item = _data[e.ItemIndex];
+
+        }
+
+        private void bClear_Click(object sender, EventArgs e)
+        {
+            _data.Clear();
+            listView1.VirtualListSize=0;
+            listView1.Refresh();
+        }
+
+        private void bStop_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+        }
+
+        private void bSave_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+
+            if (ofd.ShowDialog()!=DialogResult.OK) return;
+
+            StreamWriter outfile = 	new StreamWriter(ofd.FileName);
+            foreach(ListViewItem i in _data)
+                outfile.WriteLine(i.Text);
+            outfile.Close();
+
+        }
+
+        private void cbBaud_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+        private void cbLogToFile_CheckedChanged(object sender, EventArgs e)
+        {
+            CheckLogFile();
+        }
+
+
+        string GetLogFileName()
+        {
+            DateTime t = DateTime.Now;
+            return string.Format("LOG_{0:yyMMdd_HHmmss}.txt",t);
+        
+        }
+
+
+        void CheckLogFile()
+        {
+            if (cbLogToFile.Checked) 
+            {
+                if (logfile ==	null)
+                {
+                    logfile = new StreamWriter(GetLogFileName());
+                }
+            
+            } else
+            {
+                if (logfile!=null)
+                {
+                    logfile.Close();
+                    logfile = null;
+                }
+            }
+        
+        
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            Command("STOP");
+        }
+
+        private void buttonSend_Click(object sender, EventArgs e)
+        {
+            Command("PUT "+textBoxSend.Text+"\r\n");
+        }
+
+        private void buttonRawSend_Click(object sender, EventArgs e)
+        {
+            Command(textBoxSend.Text);
+        }
+
+
+
+    }
+}
